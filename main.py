9d0b66eb0216d3e2cc29588d854eefe5cbc699f3
RaimeiRaikiri <mikescutts10@gmail.com>
2026-06-07 21:10:36 +0100

    feat(tasks+endpoints): added task model and initial endpoints

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Task(BaseModel):
+    id: int 
+    title: str = None
+    description: None = None
+    completed: None = None
+    create_at: None = None
+    
+# Temp list for the tasks    
+tasks = ["ringo", "apple"]
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"Hello": "World "}
+@app.get("/tasks")
+def get_tasks():
+    
+    return tasks
+    
+@app.get("/tasks/{id}")
+def get_task(id):
+    None
+    
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task)
+    
+    return task
+    
+@app.put("/tasks/{id}")
+def put_task(id):
+    None
+    
+@app.delete("/tasks/{id}")
+def delete_task(id):
+    None
