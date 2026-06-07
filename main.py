b506a3496aac21b8d66eecec3a1f9a8e8ca752f8
RaimeiRaikiri <mikescutts10@gmail.com>
2026-06-07 23:05:50 +0100

    refactor(endpoints): created basic functionality for the previously added endpoints

--- main.py
+++ main.py
@@ -1,37 +1,85 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from datetime import datetime
 
 class Task(BaseModel):
-    id: int 
+    id: int = None
     title: str = None
-    description: None = None
-    completed: None = None
+    description: str = None
+    completed: bool = None
     create_at: None = None
     
+task_one = Task(
+    id=5,
+    title="test1",
+    description="testing time",
+    completed=False,
+    create_at=None
+)
+task_two = Task(
+    id=3,
+    title="test2",
+    description="testing time",
+    completed=False,
+    create_at=None
+)
+
 # Temp list for the tasks    
-tasks = ["ringo", "apple"]
+tasks = [task_one, task_two]
 
 app = FastAPI()
 
-@app.get("/tasks")
+@app.get("/tasks", response_model=list[Task])
 def get_tasks():
+    if len(tasks) > 0:
+        
+        return tasks
+    else:
+        raise HTTPException(
+            status_code=404, 
+            detail="There are no tasks found"
+        )
     
-    return tasks
+@app.get("/tasks/{task_id}", response_model=Task)
+def get_task(task_id: int):
+    if tasks[task_id]:
+        
+        return tasks[task_id]
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task {task_id} not found"
+        )
     
-@app.get("/tasks/{id}")
-def get_task(id):
-    None
-    
-@app.post("/tasks")
+@app.post("/tasks", status_code=status.HTTP_201_CREATED, response_model=Task)
 def create_task(task: Task):
     tasks.append(task)
     
     return task
     
-@app.put("/tasks/{id}")
-def put_task(id):
-    None
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task: Task):
+    try:
+        if tasks[task_id]:
+            tasks[task_id] = task
+            
+            return task
+            
+    except:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task {task_id} not found"
+        )
     
-@app.delete("/tasks/{id}")
-def delete_task(id):
-    None
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int):
+    try:
+        if tasks[task_id]:
+            tasks[task_id] = None
+            
+            return None
+    except:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task {task_id} not found"
+        )
