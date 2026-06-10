2659d44bd86caa72527e14afbfe3700b6ac24e64
RaimeiRaikiri <mikescutts10@gmail.com>
2026-06-10 22:11:54 +0100

    refactor(models): separate module for all models (excluding database SQLALchemy models)

---
+++ models.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+# For main.py
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
+    
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool
+    created_at: str | None = None
+    
+    user_id: int | None = None
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+# For Auth
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    disabled: bool | None = None
+    
+class UserInDb(User):
+    password: str
