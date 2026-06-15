bdf5b7e7faf6b24e3cb5205eb18903b5e4477cef
RaimeiRaikiri <mikescutts10@gmail.com>
2026-06-15 13:43:26 +0100

    test(pytest): added tests for get task protected endpoints

---
+++ tests/integration/test_tasks.py
@@ -0,0 +1,85 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from app.database_models import Task
+
+# Get all tasks from current user endpoints
+
+def test_get_all_tasks(test_client: TestClient, task, token):
+    
+    response = test_client.get("/tasks", headers={
+        "content-type":"application/json",
+        "Authorization":f"Bearer {token}" 
+        })
+    
+    response_json = response.json()
+    
+    assert isinstance(response_json, list)
+    
+    assert isinstance(response_json[0], dict)
+    assert response_json[0]["title"] == "test"
+    
+    assert response.status_code == status.HTTP_200_OK
+    
+def test_get_all_tasks_empty(test_client: TestClient, token, task):
+    
+    response = test_client.get("/tasks", headers={
+        "content-type":"application/json",
+        "Authorization":f"Bearer {token}" 
+        })
+    
+    response_json = response.json()
+    
+    assert isinstance(response_json, list)
+    assert response.status_code == status.HTTP_200_OK
+    
+def test_get_all_tasks_unauthenticated(test_client: TestClient, task):
+    
+    response = test_client.get("/tasks")
+    
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    
+# Get task by id endpoints
+
+def test_get_task_by_id(test_client: TestClient, token, task):
+    
+    response = test_client.get(
+        f"/tasks/{task.id}",
+        headers={
+            "Authorization":f"Bearer {token}"
+        }  )
+    response_json = response.json()
+
+    assert isinstance(response_json, dict)
+    assert response.status_code == status.HTTP_200_OK
+
+def test_get_task_by_id_unauthenticated(test_client: TestClient, task):
+    
+    response = test_client.get(f"tasks/{task.id}")
+    
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+def test_get_task_by_id_not_found(test_client: TestClient, token):
+    
+    response = test_client.get(
+        "tasks/99999",
+        headers={
+            "Authorization": f"Bearer {token}"
+        })
+    
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    
+def test_get_task_by_id_forbidden(
+    test_client: TestClient,
+    token, 
+    second_user_task,
+    second_user):
+    
+    response = test_client.get(
+        f"tasks/{second_user_task.id}",
+        headers={
+            "Authorization": f"Bearer {token}"
+        } )
+    
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    
